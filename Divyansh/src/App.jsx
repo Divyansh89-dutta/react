8e102eafba13cab3d94ef3a5434e78bd0b397308
Divyansh89-dutta <divyanshduttaroy163@gmail.com>
2025-06-26 22:49:18 +0530

    Add UseState and UserContext components; implement useContext documentation

--- Divyansh/src/App.jsx
+++ Divyansh/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Map from './components/Map'
+import UseState from './components/UseState'
 
 function App() {
   return (
     <div className='w-full h-screen text-5xl bg-blue-500'>
     <Map />
+    <UseState />
     </div>
   )
 }
---
+++ Divyansh/src/components/useContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useContext() {
+  return (
+    <div>useContext</div>
+  )
+}
+
+export default useContext
